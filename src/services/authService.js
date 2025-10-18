f638a62c0c4f5833ee13e2cb692847a5610715d8
shiva0000000001 <burramukkusivareddy99@gmail.com>
2025-10-18 09:43:47 +0530

    uploading files

---
+++ src/services/authService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+// Use environment variable set in Kubernetes or .env
+const API_URL = `${process.env.REACT_APP_BACKEND_URL}/auth`;
+
+export const login = async (username, password) => {
+  const response = await axios.post(`${API_URL}/login`, { username, password });
+  localStorage.setItem("token", response.data);
+  return response.data;
+};
+
+export const signup = async (username, email, password) => {
+  return axios.post(`${API_URL}/signup`, { username, email, password });
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+};
